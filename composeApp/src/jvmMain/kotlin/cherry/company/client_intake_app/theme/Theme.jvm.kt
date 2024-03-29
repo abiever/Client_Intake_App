702bf1b66b6f2364f782d64dc1aaed9adfe74087
aarontandem <aaronlbiever@gmail.com>
2024-03-29 18:20:54 +0900

    Initial commit

---
+++ composeApp/src/jvmMain/kotlin/cherry/company/client_intake_app/theme/Theme.jvm.kt
@@ -0,0 +1,7 @@
+package cherry.company.client_intake_app.theme
+
+import androidx.compose.runtime.Composable
+
+@Composable
+internal actual fun SystemAppearance(isDark: Boolean) {
+}
---
+++ composeApp/src/jvmMain/kotlin/main.kt
@@ -0,0 +1,17 @@
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import java.awt.Dimension
+import cherry.company.client_intake_app.App
+
+fun main() = application {
+    Window(
+        title = "Client_Intake_App",
+        state = rememberWindowState(width = 800.dp, height = 600.dp),
+        onCloseRequest = ::exitApplication,
+    ) {
+        window.minimumSize = Dimension(350, 600)
+        App()
+    }
+}
